9906cb665fa36d2394eceef24c6cdf12ea9a5ae5
Spartachus <106468069+Spartachus@users.noreply.github.com>
2022-06-04 10:50:12 +0200

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -7,15 +7,15 @@ msg = 'You Lost!'
 while attempts > 0:
     user_input = int(input('Guess a number: '))
 
-    if user_input == Sec_num:
+    elif user_input == Sec_num:
         msg = 'You Won!'
         break
-    if user_input < Sec_num:
+    elif user_input < Sec_num:
         print("Your choice is too small")
         print(f'Try again! {attempts} attempt left.')
         attempts -= 1
 
-    if user_input > Sec_num:
+    elif user_input > Sec_num:
         print("your choice is too big")
         print(f'Try again! {attempts} attempt left.')
         attempts -= 1
